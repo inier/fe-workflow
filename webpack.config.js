5e6ad7cb89ecbca78b7f2eb5eb36150257d3113f
inier <inier@qq.com>
2020-02-08 22:44:38 +0800

    fix: 优化代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,39 @@
-const path = require('path');
-const rimraf = require('rimraf');
-const Config = require('webpack-chain');
+const rimraf = require("rimraf");
+const Config = require("webpack-chain");
+const { resolve } = require("./config/utils");
+
 const config = new Config();
-const resolve = (src) => {
-  return path.join(process.cwd(), src)
-}
 
 // 删除 dist 目录
-rimraf.sync('dist')
+rimraf.sync("dist");
 
+// webpack 配置
+// 原生配置
+// module.exports = {
+//     entry: './src/index',
+//     mode: process.env.NODE_ENV,
+//     output: {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'dist'),
+//     },
+// };
+
+// 通过webpack-chain 配置
 config
   // 入口
-  .entry('src/index') 
-    .add(resolve('src/index.js'))
-    .end()
-  // 模式
-  // .mode(process.env.NODE_ENV) 等价下面
-  .set('mode', process.env.NODE_ENV)
+  .entry("index")
+  .add(resolve("src/index.js"))
+  .end()
+  // 模式 等价.mode(process.env.NODE_ENV)
+  .set("mode", process.env.NODE_ENV)
   // 出口
-  .output
-    .path(resolve('dist'))
-    .filename('[name].bundle.js');
+  .output.path(resolve("dist"))
+  .filename("[name].bundle.js");
 
 config.module
-  .rule('css')
+  .rule("css")
   .test(/\.css$/)
-    .use('css')
-    .loader('css-loader')
-      
+  .use("css")
+  .loader("css-loader");
+
 module.exports = config.toConfig();
