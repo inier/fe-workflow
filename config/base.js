3bc67c108473c786c3fb5dba8f1f62a0f9b564e4
inier <inier@qq.com>
2020-02-10 01:23:50 +0800

    fix: 更新代码

--- config/base.js
+++ config/base.js
@@ -1,19 +1,27 @@
-module.exports = (config, resolve) => {
-  return () => {
-    config
-      // 入口名称
-      .entry('app')
-      // 入口路径
-      .add(resolve('src/main.js'))
-      .end()
-      // 模式 "production" | "development" | "none"
-      // .mode(process.env.NODE_ENV) 等价下面
-      .set('mode', "development") // process.env.NODE_ENV
-      // 出口
-      .output
-      .path(resolve('dist'))
-      .filename('[name].bundle.js')
-      
-    config.devtool('cheap-source-map')
-  }
-}
+// webpack基础配置
+const path = require('path');
+const chalk = require('chalk');
+const Config = require('webpack-chain');
+const { findSync, resolve } = require('./utils');
+
+const config = new Config();
+
+const files = findSync('config/units');
+
+console.log(chalk.green(`----------------------------------------`));
+console.log(chalk.green(`   当前环境[${process.env.npm_lifecycle_event}]：${process.env.NODE_ENV}`));
+console.log(chalk.green(`----------------------------------------`));
+
+module.exports = () => {
+    const map = new Map();
+
+    // 整合units中的各项配置
+    files.map((_) => {
+        const name = path.basename(_, '.js');
+        return map.set(name, require(_)(config, resolve));
+    });
+
+    map.forEach((v) => v());
+
+    return config;
+};
