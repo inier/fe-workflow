916c8dddbaa6dbf2a6629466faf1fc9dcc42a063
inier <inier@qq.com>
2020-02-17 20:04:05 +0800

    fix: 优化ssr渲染

---
+++ scripts/base.js
@@ -0,0 +1,21 @@
+// webpack基础配置
+
+const path = require('path');
+const Config = require('webpack-chain');
+const { findSync, joinPathCWD } = require('../lib');
+
+module.exports = (options) => {
+    const files = findSync('scripts/units');
+    const config = new Config();
+    const map = new Map();
+
+    // 整合 units 中的各项配置
+    files.map((_) => {
+        const name = path.basename(_, '.js');
+        return map.set(name, require(_)({ config, resolve: joinPathCWD, options }));
+    });
+
+    map.forEach(async (v) => await v());
+
+    return config;
+};
---
+++ scripts/units/BundleAnalyzerPlugin.js
@@ -0,0 +1,16 @@
+// 打包体积分析
+// https://github.com/webpack-contrib/webpack-bundle-analyzer
+
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = ({ config, options }) => {
+    return () => {
+        if (options.report) {
+            config.plugin('BundleAnalyzerPlugin').use(BundleAnalyzerPlugin, [
+                {
+                    analyzerMode: 'static',
+                },
+            ]);
+        }
+    };
+};
